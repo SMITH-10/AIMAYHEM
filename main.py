8a40f021ac6942e6af1a7a60626c5e4f1c60bed5
Vansh Shah <75292413+VanshShah1@users.noreply.github.com>
2024-04-21 15:34:37 +0530

    xsncksbdc

--- main.py
+++ main.py
@@ -3,43 +3,59 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Zo World"
 
-    logo=ft.Text(
-        value="\z/",
-        size=30
-    )
+    chats=[]
     
-    logocont=ft.Container(
-        content=logo,
-        padding=ft.padding.Padding(left=10,right=10,top=10,bottom=0)
+    logo=ft.Container(
+        content=ft.Text(
+            value="\z/",
+            size=25,
+            color="#36d189"
+        ),
+        padding=ft.padding.Padding(left=20,right=10,top=10,bottom=0)
     )
-   
+
+    chatcont=ft.Container(
+        content=ft.Column([ft.Text("\z/", size=35, color="#36d189"),
+                           ft.ListView(controls=[])]),
+        bgcolor="black",
+        margin=ft.margin.Margin(left=310, right=200, top=50, bottom=0),
+        width=800,
+        height=600,
+        border_radius=30,
+        padding=ft.padding.Padding(left=375, right=100, top=10, bottom=0)
+    )
+
     field=ft.CupertinoTextField(
-        placeholder_text="search",
-        bgcolor=ft.colors.GREY_900,
-    )
+                placeholder_text="How may i help you?",
+                bgcolor=ft.colors.GREY_900,
+                width=800,
+                height=50,
+                border_radius=10,
+                )
 
-
-    fieldcont=ft.Container(
-        content=field,
-        margin=100
-    )
-
-    submit=ft.CupertinoButton(
-            content=ft.Text("SUBMIT", color=ft.cupertino_colors.WHITE),
-            opacity_on_click=0.1,
-            bgcolor=ft.colors.BLACK,
-            
-        )
-    submitcon=ft.Container(
-        content=submit,
-        padding=ft.padding.Padding(left=300,right=10,top=0,bottom=90)
-
-    )
-    page.add(
-        logocont,
-        fieldcont,
-        submitcon
+    submit=ft.IconButton(
+                bgcolor="#36d189",
+                icon=ft.icons.SEND_ROUNDED,
+                icon_color="white",
+                height=50,
+                width=50,
+                hover_color=ft.colors.TRANSPARENT,
+                padding=ft.padding.Padding(left=5, top=0, right=0, bottom=0)
+            )
+    
+    form=ft.Container(
+        content=ft.Row(
+                    controls=[field, submit],
+                    ),
+        padding=ft.padding.Padding(left=300, right=250, top=130, bottom=20),
         
+        )
+
+    page.add(
+        logo,
+        chatcont,
+        form
+    
     )
 
 ft.app(main)
